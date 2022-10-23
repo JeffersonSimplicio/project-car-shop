0c9cab128084fb41758e5d393adc66f975debad9
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-10-23 20:14:48 -0300

    feat: criado service de car  - seguido a interface IService

---
+++ src/interfaces/IService.ts
@@ -0,0 +1,9 @@
+interface IService<T> {
+  create(obj:unknown):Promise<T>,
+  readOne(_id:string):Promise<T>,
+  read():Promise<T[]>,
+  update(_id:string, obj:unknown): Promise<T>;
+  delete(_id:string):Promise<T>,
+}
+
+export default IService;
---
+++ src/services/Car.service.ts
@@ -0,0 +1,44 @@
+import IService from '../interfaces/IService';
+import { ICar, carZodSchema } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import { ErrorTypes } from '../errors/catalog';
+
+class CarService implements IService<ICar> {
+  private _car:IModel<ICar>;
+  constructor(model:IModel<ICar>) {
+    this._car = model;
+  }
+
+  public async create(obj:unknown): Promise<ICar> {
+    const parsed = carZodSchema.safeParse(obj);
+    if (!parsed.success) throw parsed.error;
+    return this._car.create(parsed.data);
+  }
+
+  public async readOne(_id:string): Promise<ICar> {
+    const car = await this._car.readOne(_id);
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+    return car;
+  }
+
+  public async read(): Promise<ICar[]> {
+    const cars = await this._car.read();
+    return cars;
+  }
+
+  public async update(_id: string, obj: unknown): Promise<ICar> {
+    const parsed = carZodSchema.safeParse(obj);
+    if (!parsed.success) throw parsed.error;
+    const car = await this._car.update(_id, parsed.data);
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+    return car;
+  }
+
+  public async delete(_id: string): Promise<ICar> {
+    const car = await this._car.delete(_id);
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+    return car;
+  }
+}
+
+export default CarService;
