d7374603bc24300ab928ac2f90d6fca294110e94
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-10-23 19:19:23 -0300

    refactor: alteração no formato da linha  - um maneira mais similar as demais linhas

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -4,7 +4,7 @@ const vehicleZodSchema = z.object({
   model: z.string().min(3),
   year: z.number().gte(1900).lte(2022).int(),
   color: z.string().min(3),
-  status: z.optional(z.boolean()),
+  status: z.boolean().optional(),
   buyValue: z.number().int(),
 });
 
