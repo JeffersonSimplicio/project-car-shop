fc437216d800c730db1c2fb973eeea2e641aed2c
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-10-24 15:14:25 -0300

    refactor: agora objeto possui a chave error  - objeto retornado em caso de error substituindo a chave message

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -12,7 +12,7 @@ const errorHandler: ErrorRequestHandler = (
   _next,
 ) => {
   if (err instanceof ZodError) {
-    return res.status(400).json({ message: err.issues });
+    return res.status(400).json({ error: err.issues });
   }
 
   const messageAsErrorType = err.message as keyof typeof ErrorTypes;
@@ -21,7 +21,7 @@ const errorHandler: ErrorRequestHandler = (
   const mappedError = errorCatalog[messageAsErrorType];
   if (mappedError) {
     const { httpStatus, message } = mappedError;
-    return res.status(httpStatus).json({ message });
+    return res.status(httpStatus).json({ error: message });
   }
   console.error(err);
   return res.status(500).json({ message: 'internal error' });
