5ddb6d9545058caf0b187ac72a0cafc28bb78d6b
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-10-25 20:41:04 -0300

    test: caso o id não exista, é retornado um erro

--- src/tests/integration/car.test.ts
+++ src/tests/integration/car.test.ts
@@ -32,6 +32,32 @@ const { seatsQty, ...noSeatsQty} = carMock;
 const invalidMongoId = { error: 'Id must have 24 hexadecimal characters' };
 const entityNotFound = { error: 'Object not found' };
 
+const tests = [{model: 'oi'}, {model: 8}, {year: 1899}, {year: 2023},
+{year: '2015'}, {color: 'ab'}, {color: 12}, {buyValue: 1234.56},
+{buyValue: 'texto'}, {doorsQty: 1}, {doorsQty: 5}, {doorsQty: 2.6},
+{doorsQty: 'texto'}, {seatsQty: 1}, {seatsQty: 8}, {seatsQty: 2.6},
+{seatsQty: 'texto'}];
+
+const erroMessageTest = [
+  'Should be at least 3 characters',
+  'Expected string, received number',
+  'Value should be greater than or equal to 1900',
+  'Value should be less than or equal to 2022',
+  'Expected number, received string',
+  'Should be at least 3 characters',
+  'Expected string, received number',
+  'Expected integer, received float',
+  'Expected number, received string',
+  'Value should be greater than or equal to 2',
+  'Value should be less than or equal to 4',
+  'Expected integer, received float',
+  'Expected number, received string',
+  'Value should be greater than or equal to 2',
+  'Value should be less than or equal to 7',
+  'Expected integer, received float',
+  'Expected number, received string'
+]
+
 describe('Teste da rota "/cars"', () => {
   afterEach(()=>{
     sinon.restore();
@@ -63,33 +89,7 @@ describe('Teste da rota "/cars"', () => {
     })
 
     it('Os campos devem seguir os padrões de valor',async () => {
-      const tests = [{model: 'oi'}, {model: 8}, {year: 1899}, {year: 2023},
-      {year: '2015'}, {color: 'ab'}, {color: 12}, {buyValue: 1234.56},
-      {buyValue: 'texto'}, {doorsQty: 1}, {doorsQty: 5}, {doorsQty: 2.6},
-      {doorsQty: 'texto'}, {seatsQty: 1}, {seatsQty: 8}, {seatsQty: 2.6},
-      {seatsQty: 'texto'}];
-
-      const erroMessageTest = [
-        'Should be at least 3 characters',
-        'Expected string, received number',
-        'Value should be greater than or equal to 1900',
-        'Value should be less than or equal to 2022',
-        'Expected number, received string',
-        'Should be at least 3 characters',
-        'Expected string, received number',
-        'Expected integer, received float',
-        'Expected number, received string',
-        'Value should be greater than or equal to 2',
-        'Value should be less than or equal to 4',
-        'Expected integer, received float',
-        'Expected number, received string',
-        'Value should be greater than or equal to 2',
-        'Value should be less than or equal to 7',
-        'Expected integer, received float',
-        'Expected number, received string'
-      ]
       const create = sinon.spy(Model, 'create');
-
       for(let i = 0; i < tests.length; i += 1) {
         const response = await chai
           .request(app)
@@ -175,6 +175,18 @@ describe('Teste da rota "/cars"', () => {
       expect(response.body)
         .to.be.an('object')
         .to.be.deep.equal(carMockUpdateWithId);
+    })
+
+    it('É retornado um erro caso não tenha um carro correspondência', async () => {
+      sinon.stub(Model, 'findByIdAndUpdate').resolves(null);
+      const response = await chai
+        .request(app)
+        .put(`/cars/${nonExistentId}`)
+        .send(carMockUpdate);
+      expect(response.status).to.be.equal(404);
+      expect(response.body)
+        .to.be.an('object')
+        .to.be.deep.equal(entityNotFound);
     })
   })
 })
