378b329a557681a65b943fb29999531688e99783
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-10-25 16:14:23 -0300

    test: Possível criar carro sem campos obrigatórios

--- src/tests/integration/car.test.ts
+++ src/tests/integration/car.test.ts
@@ -4,7 +4,7 @@ import chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 import 'dotenv/config';
-// import { Response } from 'superagent';
+import { Response } from 'superagent';
 import app from '../../app';
 import { Model } from 'mongoose';
 import {
@@ -20,6 +20,13 @@ import {
 
 chai.use(chaiHttp);
 const { expect } = chai;
+
+const { model, ...noModel} = carMock;
+const { year, ...noYear} = carMock;
+const { color, ...noColor} = carMock;
+const { buyValue, ...noBuyValue} = carMock;
+const { doorsQty, ...noDoorsQty} = carMock;
+const { seatsQty, ...noSeatsQty} = carMock;
 
 describe('Teste da rota "/cars"', () => {
   afterEach(()=>{
@@ -38,6 +45,45 @@ describe('Teste da rota "/cars"', () => {
       expect(response.body)
         .to.be.an('object')
         .and.to.deep.equal(carMockWithId);
+    })
+
+    it('Não é possível criar um novo carro, sem algum dos campos obrigatórios', async () => {
+      const create = sinon.spy(Model, 'create');
+
+      let response = await chai.request(app).post('/cars').send({});
+      expect(response.status).to.be.equal(400);
+      expect(create.notCalled).to.be.true;
+
+
+      // Sem 'model'
+      response = await chai.request(app).post('/cars').send(noModel);
+      expect(response.status).to.be.equal(400);
+      expect(create.notCalled).to.be.true;
+
+      // Sem 'color'
+      response = await chai.request(app).post('/cars').send(noModel);
+      expect(response.status).to.be.equal(400);
+      expect(create.notCalled).to.be.true;
+
+      // Sem 'buyValue'
+      response = await chai.request(app).post('/cars').send(noBuyValue);
+      expect(response.status).to.be.equal(400);
+      expect(create.notCalled).to.be.true;
+
+      // Sem 'year'
+      response = await chai.request(app).post('/cars').send(noYear);
+      expect(response.status).to.be.equal(400);
+      expect(create.notCalled).to.be.true;
+
+      // Sem 'doorsQty'
+      response = await chai.request(app).post('/cars').send(noDoorsQty);
+      expect(response.status).to.be.equal(400);
+      expect(create.notCalled).to.be.true;
+
+      // Sem 'seatsQty'
+      response = await chai.request(app).post('/cars').send(noSeatsQty);
+      expect(response.status).to.be.equal(400);
+      expect(create.notCalled).to.be.true;
     })
   })
 })
