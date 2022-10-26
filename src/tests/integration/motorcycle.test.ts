d6911261bd4c7fae4c474a15f3b22ed62f0c10dd
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-10-26 10:17:19 -0300

    test: moto só é criada dentro dos padrões

--- src/tests/integration/motorcycle.test.ts
+++ src/tests/integration/motorcycle.test.ts
@@ -36,7 +36,23 @@ const dataTests = [{model: 'oi'}, {model: 8}, {year: 1899}, {year: 2023},
 
 const incompleteMocks = [{}, noModel, noColor, noBuyValue, noYear, noCategory, noEngineCapacity];
 
-const erroMessageTest = [];
+const erroMessageTest = [
+  'Should be at least 3 characters',
+  'Expected string, received number',
+  'Value should be greater than or equal to 1900',
+  'Value should be less than or equal to 2022',
+  'Expected number, received string',
+  'Should be at least 3 characters',
+  'Expected string, received number',
+  'Expected integer, received float',
+  'Expected number, received string',
+  `Invalid enum value. Expected 'Street' | 'Custom' | 'Trail', received 1`,
+  `Invalid enum value. Expected 'Street' | 'Custom' | 'Trail', received 'texto'`,
+  'Value should be greater than 0',
+  'Value should be less than or equal to 2500',
+  'Expected integer, received float',
+  'Expected number, received string'
+];
 
 describe.only('Teste da rota "/motorcycles"', () => {
   afterEach(()=>{
@@ -66,6 +82,21 @@ describe.only('Teste da rota "/motorcycles"', () => {
           .post('/motorcycles')
           .send(incompleteMocks[i]);
         expect(response.status).to.be.equal(400);
+        expect(create.notCalled).to.be.true;
+      }
+    })
+
+    it('Os campos devem seguir os padrões de valor',async () => {
+      const create = sinon.spy(Model, 'create');
+      for(let i = 0; i < dataTests.length; i += 1) {
+        const response = await chai
+          .request(app)
+          .post('/motorcycles')
+          .send({ ...motorcycleMock, ...dataTests[i]});
+        expect(response.status).to.be.equal(400);
+        const erroMessage = response.body.error[0].message;
+        expect(typeof(erroMessage) === 'string').to.be.true;
+        expect(erroMessage).to.be.equal(erroMessageTest[i]);
         expect(create.notCalled).to.be.true;
       }
     })
